7fc7b36cda70455370da6d3bcf33dfae8403470b
Golden Eagle <joowoon.kang@biola.edu>
2020-07-19 12:20:57 -0700

    speed working and handleProgress is now working you can see the video progress now.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -28,10 +28,26 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip); //pasrse turns to true number
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.value);
+  console.log(this.name);
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
 //hook up the event listener
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
